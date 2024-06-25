24e39a5231000b7e51af9d8d8777a130e10cba72
Fun6789 <aksdav05@gmail.com>
2024-06-26 01:47:03 +0530

    Adding Implict Intent.

--- app/src/main/java/com/example/implictorexplictintent/MainActivity.kt
+++ app/src/main/java/com/example/implictorexplictintent/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.implictorexplictintent
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -14,8 +17,19 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         val button= findViewById<Button>(R.id.button)
+        val webButton= findViewById<CardView>(R.id.website)
+        val camera = findViewById<CardView>(R.id.camera)
         button.setOnClickListener {
-            intent= Intent(applicationContext,SecondActivity::class.java)
+         intent= Intent(applicationContext,SecondActivity::class.java)
+            startActivity(intent)
+        }
+        webButton.setOnClickListener {
+            val intent= Intent(Intent.ACTION_VIEW)
+            intent.data= Uri.parse("https://github.com/Fun6789")
+            startActivity(intent)
+        }
+        camera.setOnClickListener {
+            val intent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivity(intent)
         }
     }
